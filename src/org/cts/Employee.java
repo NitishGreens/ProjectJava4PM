41e54e6299509738435328a375db0a04cb6a0ca3
NitishGreens <nitishGreens@gmail.com>
2021-12-28 16:38:30 +0530

    task done

---
+++ src/org/cts/Employee.java
@@ -0,0 +1,15 @@
+package org.cts;
+
+public class Employee {
+
+	public void add() {
+		System.out.println("add");
+	}
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.add();
+
+	}
+
+}
